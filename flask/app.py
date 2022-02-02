700ce312d4ee7373db738cf27ebd69ae1f8c1021
Joe M <46854039+josephmanasa@users.noreply.github.com>
2022-02-02 14:38:21 -0500

    expanded page templating; moved analyzer to dedicated page; added tutorial page

--- flask/app.py
+++ flask/app.py
@@ -25,7 +25,7 @@ except: maxSize = 200
 class MainForm(FlaskForm):
   file = FileField('Select file. Currently supported file types are CSV and Excel; maximum data size accepted has been set to ' + str(maxSize) + ' points.')
   maxdim = SelectField('Maximum dimension', choices=[(2, '2'), (1, '1'), (0, '0')])
-  coeff = SelectField('Field Coefficient (7 is recommended for 3-dimensional data)', choices=[(7, '7'), (3, '3'), (2, '2')])
+  coeff = SelectField('Field coefficient (7 is recommended for 3-dimensional data)', choices=[(7, '7'), (3, '3'), (2, '2')])
   igLabels = BooleanField('Ignore first row (check this if the first row contains, say, labels)')
   igEnum = BooleanField('Ignore first column (check this if the first column contains, say, the enumeration index)')
   delimiter = SelectField('Delimiter (if applicable; the comma is the most common delimiter if unsure)', choices=[(',', ','), (';', ';'), (':', ':'),(' ', ' '), ('|','|'), ('\t', 'tab')])
@@ -40,13 +40,21 @@ captcha = FlaskSessionCaptcha(app)
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+  return render_template('index.html', title="Home")
+
+@app.route('/instructions', methods=['GET', 'POST'])
+def instructions():
+  return render_template('instructions.html', title="How to use the analyzer")
+
+@app.route('/analyzer', methods=['GET', 'POST'])
+def analyzer():
   form = MainForm()
   if form.submit.data:
     if captcha.validate():
       try: upload = form.file.data.read()
       except:
         flash('Please select a file.')
-        return render_template('index.html', form=form)
+        return render_template('analyzer.html', title="Analyzer", form=form)
       co = copy(upload)
       type = from_buffer(co).upper()
       if 'ASCII' in type or 'CSV' in type or 'TEXT' in type:
@@ -57,20 +65,20 @@ def index():
         f = io.BufferedReader(io.BytesIO(upload))
       else:
         flash('File type not yet supported or file format error.')
-        return render_template('index.html', form=form)
+        return render_template('analyzer.html', title="Analyzer", form=form)
       try:
         result = ctda.analyze(f, t, int(form.maxdim.data), int(form.coeff.data), form.delimiter.data, form.lineterminator.data, form.igLabels.data, form.igEnum.data, maxSize)
         if len(result) == 1:
           flash(result[0])
-          return render_template('index.html', form=form)
+          return render_template('analyzer.html', title="Analyzer", form=form)
         image = base64.b64encode(result[0].getvalue()).decode('utf-8')
         bar = base64.b64encode(result[1].getvalue()).decode('utf-8')
-        return render_template('results.html', diagram=image, barcode=bar, betti=result[2], stats=result[3], raw=result[4], dim=result[5])
+        return render_template('results.html', title="Results", diagram=image, barcode=bar, betti=result[2], stats=result[3], raw=result[4], dim=result[5])
       except:
         flash('There seems to a miscalculation.')
-        return render_template('index.html', form=form)
+        return render_template('analyzer.html', title="Analyzer", form=form)
     else:
       flash('Captcha incorrect.')
-      return render_template('index.html', form=form)
-  return render_template('index.html', form=form)
+      return render_template('analyzer.html', title="Analyzer", form=form)
+  return render_template('analyzer.html', title="Analyzer", form=form)
 
