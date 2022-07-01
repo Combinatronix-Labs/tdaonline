fb82c636bb634d6c3078ca2a3c45d80d63940aa8
thMthMnky <12237673+thMthMnky@users.noreply.github.com>
2022-07-01 15:31:39 -0400

    Adding styles to the Results page

--- flask/static/js/main.js
+++ flask/static/js/main.js
@@ -5,3 +5,58 @@ const toggleMobileMenu = (menu) => {
 if (window.history.replaceState) {
   window.history.replaceState(null, null, window.location.href);
 }
+
+const collapseHeader = (toggler, table, className) => {
+  const headRow = table.getElementsByClassName(className)[0];
+  if (headRow.classList.contains('active')) {
+    headRow.classList.remove('active');
+    toggler.setAttribute('value', 'Hide');
+  } else {
+    headRow.classList.add('active');
+    toggler.setAttribute('value', 'Show');
+  }
+}
+
+const collapseBody = (table, className) => {
+
+  const bodyRows = table.getElementsByClassName(className);
+  for (var i = 0; i < bodyRows.length; i++) {
+    const bodyRow = bodyRows[i];
+    if (bodyRow.classList.contains('active')) {
+      bodyRow.classList.remove('active');
+    } else {
+      bodyRow.classList.add('active');
+    }
+  }
+}
+const collapseBettiAndEulerTable = (toggler) => {
+  element = document.getElementById('BettiAndEulerTable');
+
+  collapseHeader(toggler, element, 'BettiAndEulerHeadRow');
+
+  collapseBody(element, 'BettiAndEulerBodyRow');
+}
+
+const collapseStatisticsTable = (toggler) => {
+  element = document.getElementById('StatisticsTable');
+
+  collapseHeader(toggler, element, 'StatisticsHeadRow');
+
+  collapseBody(element, 'StatisticsBodyRow');
+}
+
+const collapseRatiosTable = (toggler) => {
+  element = document.getElementById('RatiosTable');
+
+  collapseHeader(toggler, element, 'RatiosHeadRow');
+
+  collapseBody(element, 'RatiosBodyRow');
+}
+
+const collapseRawDataTable = (toggler) => {
+  element = document.getElementById('RawDataTable');
+
+  collapseHeader(toggler, element, 'RawDataHeadRow');
+
+  collapseBody(element, 'RawDataBodyRow');
+}
